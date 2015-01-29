c6c10567936d5f62d29f2b1457001e8f19b4699f
sy6765856 <sy6765856@gmail.com>
2015-01-29 18:13:24 +0800

    add point light

--- src/app.js
+++ src/app.js
@@ -5,20 +5,41 @@
  * Time: 下午3:18
  * To change this template use File | Settings | File Templates.
  */
-var objects = (function (_) {
+var lights = (function(_) {
+    var pointLight = new _.PointLight(0xFFFFFF);
+    pointLight.position.x = 10;
+    pointLight.position.y = 50;
+    pointLight.position.z = 130;
+    var lights = [];
+    lights.push({
+        body: pointLight,
+        behavior: function(){
+        }
+    });
+    return lights;
+})(THREE);
+
+var objects = (function (_, lights) {
     var geo = new _.BoxGeometry(1,1,1),
-        material = new _.MeshBasicMaterial( { color: 0x00ff00 }),
+        material = new _.MeshLambertMaterial( { color: 0xCC0000 }),
         mesh = new _.Mesh(geo, material);
     var objs = [];
     objs.push({
         body: mesh,
+        xx:0.01,
+        yy:0.01,
         behavior: function(){
-            this.body.position.x+=0.01;
-            this.body.position.y+=0.01;
+            this.body.scale.x += this.xx;
+            this.body.scale.y -= this.yy;
+            this.xx -= this.body.scale.x/this.body.scale.y*0.01;
+            this.yy -= this.body.scale.x/this.body.scale.y*0.01;
+            console.log(this.xx, this.yy);
         }
     });
+    objs = objs.concat(lights);
     return objs;
-})(THREE);
+})(THREE, lights);
+
 var scene = (function (_, objs) {
     var scene = new _.Scene;
     for(var key in objs) {
@@ -37,7 +58,12 @@ var renderer = (function (_){
 var camera = (function (_){
     var camera = new _.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000);
     camera.position.z = 5;
-    return camera;
+    return {
+        body : camera,
+        behavior : function() {
+
+        }
+    };
 })(THREE);
 
 var app = (function(s,c,r,objs) {
@@ -46,7 +72,8 @@ var app = (function(s,c,r,objs) {
         for(var key in objs) {
             objs[key].behavior.apply(objs[key]);
         }
-        r.render(s, c);
+        c.behavior();
+        r.render(s, c.body);
     };
     return {
         run: this.run
